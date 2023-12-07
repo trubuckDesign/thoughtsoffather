0409ac5d7b6499268cb3ccd63db186496174f50a
Ryan Buck <preludeofme@gmail.com>
2023-12-07 10:27:29 -0600

    reset button done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState, useRef, useCallback } from "react";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import JournalButton from "@/components/buttons/journalButton";
 import { CSSTransition } from "react-transition-group";
 import "../css/transitions.css";
@@ -19,6 +19,7 @@ const LandingPage = () => {
   const [thoughts, setThoughts] = useState<Thoughts[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [showContinuePrompt, setShowContinuePrompt] = useState(false);
+  const [showStartFromBeginningButton, setShowStartFromBeginningButton] = useState(false);
 
   const [hasMore, setHasMore] = useState(true);
   const [lastVisiblePostId, setLastVisiblePostId] = useState<number>(1);
@@ -27,15 +28,23 @@ const LandingPage = () => {
     setShowContinuePrompt(false);
     const lastReadPostId = parseInt(localStorage.getItem("lastReadThoughtId") || "0");
     setLastVisiblePostId(lastReadPostId);
-    // Modify your fetching logic to start from lastReadPostId
-    // You may need to adjust your API or fetching logic to accommodate this
+    setShowStartFromBeginningButton(true); // Show the button when continuing
+
     fetchMoreData(lastReadPostId);
   };
   const startFromBeginning = () => {
     setShowContinuePrompt(false);
     localStorage.removeItem("lastReadThoughtId");
     setLastVisiblePostId(1);
-    fetchMoreData();
+    setShowStartFromBeginningButton(false);
+    fetchMoreData(1);
+  };
+  const resetAndFetchFromStart = async () => {
+    setShowStartFromBeginningButton(false);
+    setThoughts([]); // Clear current posts
+    setHasMore(true); // Reset hasMore state
+    setLastVisiblePostId(1); // Reset last visible post ID
+    await fetchMoreData(1); // Fetch from the first post
   };
 
   useEffect(() => {
@@ -52,27 +61,25 @@ const LandingPage = () => {
     setOpen(!isOpen);
   };
 
-  const fetchMoreData = async (lastReadPostId?: number) => {
-    if (!hasMore) return;
-    setIsLoading(true); // Set isLoading to true at the beginning of data fetch
-    let lastPostId = 0;
-    // Using the ID of the last post to determine the start of the next fetch
-    if (lastReadPostId) {
-      lastPostId = lastVisiblePostId;
-    } else {
-      lastPostId = thoughts.length > 0 ? thoughts[thoughts.length - 1].thoughtId : 0;
-    }
+  const fetchMoreData = async (startId?: number) => {
+    if (!hasMore || isLoading) return;
+    setIsLoading(true);
+
+    // Use startId if provided, otherwise calculate based on the current posts
+    let fetchStartId = startId ?? (thoughts.length > 0 ? thoughts[thoughts.length - 1].thoughtId + 1 : 0);
 
     try {
-      console.log("loading posts startId:", lastPostId, lastReadPostId, lastVisiblePostId);
-      const response = await fetch(`/api/thoughts?startId=${lastPostId}&thoughtCount=${POSTS_PER_PAGE}`);
+      const response = await fetch(`/api/thoughts?startId=${fetchStartId}&thoughtCount=${POSTS_PER_PAGE}`);
       const data = await response.json();
-      setThoughts(thoughts.concat(data.posts));
+
+      // If starting fresh, replace thoughts, otherwise append
+      const newThoughts = startId === 1 ? data.posts : [...thoughts, ...data.posts];
+      setThoughts(newThoughts);
       setHasMore(data.posts.length === POSTS_PER_PAGE);
     } catch (error) {
       console.error("Error loading more posts:", error);
     } finally {
-      setIsLoading(false); // Set isLoading to true at the beginning of data fetch
+      setIsLoading(false);
     }
   };
 
@@ -91,18 +98,27 @@ const LandingPage = () => {
       </CSSTransition>
 
       <CSSTransition in={isOpen} timeout={1900} classNames="fade" unmountOnExit>
-        <Box sx={{ overflowY: "auto", maxHeight: "95vh", width: "100vw", padding: "20px 0" }}>
-          {thoughts.map((thought, index) => (
-            <Box key={index}>
-              <ThoughtPage thought={thought} setLastVisiblePostId={setLastVisiblePostId} />
-            </Box>
-          ))}
-          {hasMore && (
-            <div ref={(el) => setTarget(el)}>
-              <HandwritingSpinner />
-            </div>
-          )}
-        </Box>
+        <>
+          <Box sx={{ overflowY: "auto", maxHeight: "95vh", width: "100vw", padding: "20px 0" }}>
+            {showStartFromBeginningButton && (
+              <Box sx={{ position: "relative", marginLeft: "auto", display: "flex", justifyContent: "center" }}>
+                <Button variant="contained" onClick={resetAndFetchFromStart}>
+                  Start from Beginning
+                </Button>
+              </Box>
+            )}
+            {thoughts.map((thought, index) => (
+              <Box key={index}>
+                <ThoughtPage thought={thought} setLastVisiblePostId={setLastVisiblePostId} />
+              </Box>
+            ))}
+            {hasMore && (
+              <div ref={(el) => setTarget(el)}>
+                <HandwritingSpinner />
+              </div>
+            )}
+          </Box>
+        </>
       </CSSTransition>
       <ContinueReadingDialog
         continueFromLastRead={continueFromLastRead}
