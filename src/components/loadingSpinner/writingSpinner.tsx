69d68f8f8f6e6f2476b4034d3bc1f6ab53359b8f
Ryan Buck <preludeofme@gmail.com>
2023-12-08 23:49:35 -0600

    updated progress spinner

--- src/components/loadingSpinner/writingSpinner.tsx
+++ src/components/loadingSpinner/writingSpinner.tsx
@@ -1,25 +1,15 @@
+import { Avatar, Box, Card, CardHeader, IconButton, Skeleton } from "@mui/material";
 import React from "react";
 import { useSpring, animated } from "react-spring";
 
 const HandwritingSpinner = () => {
-  const props = useSpring({
-    strokeDashoffset: 0,
-    from: { strokeDashoffset: 1000 },
-    config: { duration: 2000 },
-    reset: true,
-    loop: true,
-  });
-
   return (
-    <svg width="200" height="100" viewBox="0 0 200 100">
-      <path id="text-path" d="M10,50 C100,0 100,100 190,50" fill="transparent" />
-      <text>
-        <textPath href="#text-path" startOffset="0%">
-          Handwriting Effect
-        </textPath>
-      </text>
-      <animated.path d="M10,50 C100,0 100,100 190,50" fill="transparent" stroke="black" strokeWidth="2" strokeDasharray="1000" style={props} />
-    </svg>
+    <Card sx={{ maxWidth: 345, m: 2 }}>
+      <CardHeader
+        title={<Skeleton animation="wave" height={10} width="80%" style={{ marginBottom: 6 }} />}
+        subheader={<Skeleton animation="wave" height={10} width="40%" />}
+      />
+    </Card>
   );
 };
 
