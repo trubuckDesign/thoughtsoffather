062067821b209cedb44eeff5d4b1c061704eebfc
Ryan Buck <preludeofme@gmail.com>
2024-01-24 23:46:35 -0600

    dynamic routes working, need to create links and fix landing page

--- src/app/[thoughtId]/page.tsx
+++ src/app/[thoughtId]/page.tsx
@@ -1,10 +1,41 @@
 "use client";
-import React, { useState } from "react";
+// pages/[thoughtId].tsx
+import { withRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import LandingPage from "@/components/landingPage";
+import { Thoughts } from "@prisma/client";
+import { usePathname } from "next/navigation";
 
-import { Box } from "@mui/material";
+export default function Page({ params }: { params: { thoughtId: string } }) {
+  console.log("pathname", params.thoughtId);
+  // const router = useRouter();
+  // const thoughtId = router.query.thoughtId;
+  const [post, setPost] = useState<Thoughts | null>(null);
 
-const AddPostPage = () => {
-  return <Box sx={{}}></Box>;
-};
+  async function fetchPostById(id: string): Promise<Thoughts | null> {
+    try {
+      const response = await fetch(`/api/thoughts/${params.thoughtId}`); // Adjust the URL to match your API endpoint
+      if (!response.ok) {
+        throw new Error("Post not found");
+      }
+      const post: Thoughts = await response.json();
+      return post;
+    } catch (error) {
+      console.error("Error fetching post:", error);
+      return null;
+    }
+  }
 
-export default AddPostPage;
+  useEffect(() => {
+    if (params.thoughtId) {
+      fetchPostById(params.thoughtId as string).then(setPost);
+    }
+  }, [params.thoughtId]);
+
+  if (!post) {
+    return <div>Loading...</div>; // Replace with your loading component
+  }
+
+  return <LandingPage initialPosts={[post]} />;
+}
